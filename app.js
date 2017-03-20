d7769813f540f7a60c5442850c2c3b715898bd12
Katarzyna Waszczeniuk <kmarkon@gmail.com>
2017-03-20 22:42:09 +0100

    add servicios submenu

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 //pojawienie się introdaction
 $(function(){
-var $p=$(".introduction p")
-  console.log($p)
+var $p=$(".introduction p");
   $p.slideToggle( 2000, "linear" );
 
-// servicios
-var $serviciosLi=$('.serviciosList');
-var $servicios=$('.servicios').on("mouseenter", function(){
-  $serviciosLi.slideDown();
-});
-var $servicios=$('.servicios').on("mouseleave", function(){
-  $serviciosLi.slideUp();
-});
-
-
-
-
-
-
-
-
-
+// slideToggle lists
+var $mainLi=$('.mainLi').find("li")
+$mainLi.on('click',function(){
+//   console.log('aaa')
+  $(this).children().slideToggle();
 
 });
+
+
+
+  })
