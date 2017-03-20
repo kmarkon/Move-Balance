75140875b60292125e04ebfd07571cceac5aba80
Katarzyna Waszczeniuk <kmarkon@gmail.com>
2017-03-20 13:44:04 +0100

    add sercicios

--- app.js
+++ app.js
@@ -3,4 +3,23 @@ $(function(){
 var $p=$(".introduction p")
   console.log($p)
   $p.slideToggle( 2000, "linear" );
+
+// servicios
+var $serviciosLi=$('.serviciosList');
+var $servicios=$('.servicios').on("mouseenter", function(){
+  $serviciosLi.slideDown();
+});
+var $servicios=$('.servicios').on("mouseleave", function(){
+  $serviciosLi.slideUp();
+});
+
+
+
+
+
+
+
+
+
+
 });
