a251eba71f27a8028620e05a2939f1bb86d9dffa
Katarzyna Waszczeniuk <kmarkon@gmail.com>
2017-04-05 14:27:27 +0200

    mod. section2 i 4

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ $(function(){
 
 // slideToggle lists
 var $mainLi=$('.mainLi').find("li")
-// $mainLi.on('click',function(){
-//   $(this).children().slideToggle();
-// });
-$mainLi.on('mouseenter',function(){
-  $(this).children().slideDown();
-});
-$mainLi.on('mouseleave',function(){
-  $(this).children().slideUp();
-});
+  $mainLi.on('mouseenter',function(){
+    $(this).children().slideDown();
+  });
+  $mainLi.on('mouseleave',function(){
+    $(this).children().slideUp();
+  });
 
 // sticky menu
 var $header=$("header");
@@ -25,27 +22,28 @@ $(window).scroll(function (event){
     $header
     .addClass("sticky")
     .css("background-color",'white');
-    // $Logo/*zmiana logo na mniejsze*/
-    // .css("background-image", 'url("images/move-balance2.png")')
-    // .addClass('newLogo')
-    // .css('height','100%');
   }else{
     $header.removeClass("sticky");
-    // $Logo
-    // .css("background-image", 'url("images/move-balance.png")')
-    // css.removeClass('newLogo');
-    // .css('height','100%');
   };
 });
 
 // sections2, section4
-var $section2=$(".section2");
+  var $section2=$(".section2");
     $section2.hide();
     $section2.show(1500);
-var $section4=$(".section4");
+
+  var $section4=$(".section4");
     $section4.hide();
-    $section4.show(1500);
+
+  var $Section4position=$(".section4").offset().top;/*pobieranie pozycji elementu*/
+    $(window).scroll(function(){
+      if (document.body.scrollTop >$Section4position|| document.documentElement.scrollTop > $Section4position) {
+            $section4.show(1500);
+    };
+});/*koniec scroll section2 i section4*/
 
 
 
-  })
+
+
+  })/*koniec function*/
