a372ec6a2f0f5b89bd623cff5340d28203ac252a
Katarzyna Waszczeniuk <kmarkon@gmail.com>
2017-04-10 14:51:12 +0200

    servicos,nosotros

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 $(function(){
 
-
-// slideToggle lists
+/*slideToggle lists*/
 var $mainLi=$('.mainLi').find("li")
   $mainLi.on('mouseenter',function(){
     $(this).children().slideDown();
@@ -10,7 +9,7 @@ var $mainLi=$('.mainLi').find("li")
     $(this).children().slideUp();
   });
 
-// sticky menu
+/*sticky menu*/
 var $header=$("header");
 var $Top=$header.offset();
 var $Top = $Top.top;
@@ -26,8 +25,17 @@ $(window).scroll(function (event){
     $header.removeClass("sticky");
   };
 });
+/*section1_servicios, section1_nosotros*/
+var $section1_servicios=$(".section1_servicios");
+  $section1_servicios.hide();
+  $section1_servicios.show(1500);
 
-// sections2, section4
+  var $section1_nosotros=$(".section1_nosotros");
+  $section1_nosotros.hide();
+  $section1_nosotros.show(1500);
+
+/*sections2, section4*/
+
   var $section2=$(".section2");
     $section2.hide();
     $section2.show(1500);
@@ -38,12 +46,50 @@ $(window).scroll(function (event){
   var $Section4position=$(".section4").offset().top;/*pobieranie pozycji elementu*/
     $(window).scroll(function(){
       if (document.body.scrollTop >$Section4position|| document.documentElement.scrollTop > $Section4position) {
-            $section4.show(1500);
+            $section4.show(2000);
     };
-});/*koniec scroll section2 i section4*/
+});/*koniec scroll section4*/
+
+/*Panel*/
+// var $bar=$("#bars");
+// var $Panel=$(".Panel");
+//   var $PanelWidth=$Panel.width();/*to get width of mobile menu*/
+//
+//   $Panel.css("left",-$PanelWidth);
+//
+//
+//     $bar.on('click', function(){
+//       // $Panel.css("left","0");
+//       $Panel.addClass("PanelAnimation");
+//
+//     });
+
+
 
 
 
 
 
   })/*koniec function*/
+
+  document.addEventListener("DOMContentLoaded", function(){
+  /*slider nosotros*/
+
+  var slideIndex=0;
+  carousel();
+  function carousel(){
+    var i;
+    var li=document.querySelectorAll(".slider_nosotros li");
+
+      for(i=0; i<li.length; i++){
+        li[i].style.display="none";
+      }
+      slideIndex++;
+      if(slideIndex>li.length){
+        slideIndex=1;
+      }
+      li[slideIndex-1].style.display="block";
+      setTimeout(carousel,2500);
+    }
+
+  })
